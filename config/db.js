67743af86ffc8cd5d0d60e35bd3b539682f9f353
AriKrnwan <ari.20057@mhs.unesa.ac.id>
2024-08-21 19:01:21 +0700

    P

--- config/db.js
+++ config/db.js
@@ -1,11 +1,17 @@
 // config/db.js
 const mysql = require('mysql2/promise');
 
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'web_pelayanan'
+// });
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'web_pelayanan'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME
 });
 
 module.exports = pool;
