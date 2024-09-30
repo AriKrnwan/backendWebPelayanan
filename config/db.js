6476c621117ed6f4090f029d6d9fb30cd786a93e
AriKrnwan <ari.20057@mhs.unesa.ac.id>
2024-09-30 20:59:24 +0700

    P

--- config/db.js
+++ config/db.js
@@ -1,33 +1,20 @@
 // config/db.js
 const mysql = require('mysql2/promise');
 
-// const pool = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'web_pelayanan'
-// });
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'web_pelayanan'
 });
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DBNAME
+// });
 
 module.exports = pool;
 
 
 
-
-// const pool = mysql.createPool({
-//   host: 'bakgghhx5etsvqz4hp6b-mysql.services.clever-cloud.com',
-//   user: 'ulyjyzwpgkovityk',
-//   password: 'rwZJ2flVCYLUVUUl6yZ0',
-//   database: 'bakgghhx5etsvqz4hp6b'
-// });
-
-// host: 'bakgghhx5etsvqz4hp6b-mysql.services.clever-cloud.com',
-//   user: 'ulyjyzwpgkovityk',
-//   password: 'rwZJ2flVCYLUVUUl6yZ0',
-//   database: 'bakgghhx5etsvqz4hp6b',
-//   port: 3306
